1ed492cd7a8d8db91ae538f3bcc93d67472cdb18
Abraheem97 <abraheem.asad@devsinc.com>
2020-04-16 17:04:31 +0500

    Initial commit, added home page component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React, { PureComponent } from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <React.Fragment>
+    {" "}
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SHIRTS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+export default HomePage;
